5da95bc1d64a3eeef6e16a0eedb902688845b68f
Kristin Blacklock (Khare) kmb413@scarletmail.rutgers.edu <kristin.blacklock@rutgers.edu>
2018-05-10 12:02:02 -0400

    Added corret 1cv2 pdbs for scoring, 1cv2 constraints for bottom, test scripts to check csts, HIP params file because there's a doubly protonated HIS

---
+++ env.sh
@@ -0,0 +1,6 @@
+export WORKDIR=/home/kmb413/2018_Damborsky_HAD/
+export SCRIPTS=$WORKDIR/scripts
+export ROSETTA_BIN="/home/kmb413/RealRosetta/Rosetta/main/source/bin"
+export ROSETTA_EXEC="rosetta_scripts.linuxclangrelease"
+export OLD_ROSETTA_BIN="/home/kmb413/Rosetta/main/source/bin"
+export MATCHER="match.static.linuxgccrelease"
